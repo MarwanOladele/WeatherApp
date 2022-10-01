3045338f694d852f6fc67e87a77a1a0ce8c53998
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-01 16:37:42 +0100

    new commit

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,22 +7,22 @@ const Search = ({onSearchChange}) => {
   const [search, setsearch] = useState(null)
 
   const loadOptions = (inputValue) => {
-    fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-	  .then(response => response.json())
-	  .then(response => {
-      return {
-        options: response.data.map((city) => {
-          return {
-            value: '',
-            label: ''
-          }
-        })
-      }
-    })
-	  .catch(err => console.error(err));
-
-  }
-
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+  };
 
   const handleChange = (searchData) => {
     setsearch(searchData) 
