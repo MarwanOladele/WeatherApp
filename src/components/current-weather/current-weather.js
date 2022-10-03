7edae06e9520eff873b3ab7ab003ca1e1884d1fb
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-03 17:34:28 +0100

    new commit

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -11,6 +11,30 @@ const CurrentWeather = () => {
         </div>
         <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">20 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
