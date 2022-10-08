43c02f589a330f0c99099828555b09b8f4830cf2
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-08 17:38:26 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,13 +1,13 @@
 import './current-weather.css'
 
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
         <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
