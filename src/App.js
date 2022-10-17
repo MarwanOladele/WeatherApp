4f8fa9b953314127c94aa79e4581d64d897f1f4f
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-17 18:09:00 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
-      <Forecast />
+      {forecastWeather && <Forecast data={forecastWeather} />}
     </div>
   );
 }
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
-const Forecast = () => {
+const Forecast = ({ data }) => {
   return (
-    <div>Forect</div>
-  )
-}
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((items, i) => (
+          <AccordionItem key={i}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Hello</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
 
-export default Forecast
+export default Forecast;
