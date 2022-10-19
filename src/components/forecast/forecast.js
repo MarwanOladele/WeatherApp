db6c2544417d14db7615b52dfcdfe85b065f3e64
Oladele Marwan <oladelemarwan@gmail.com>
2022-10-19 17:47:51 +0100

    new commit

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -7,15 +7,30 @@ import {
   AccordionItemPanel,
 } from "react-accessible-accordion";
 
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 const Forecast = ({ data }) => {
+
+
+  const currentDay = new Date().getDay()
+  const forecastDay = WEEK_DAYS.slice(currentDay, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, currentDay))
+  console.log(forecastDay);
+
   return (
     <>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((items, i) => (
+        {data.list.splice(0, 7).map((item, i) => (
           <AccordionItem key={i}>
             <AccordionItemHeading>
-              <AccordionItemButton>Hello</AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-items">
+                  <img src={`icons/${item.weather[0].icon}.png`} alt="weather" className="icon-small" />
+                  <label className="day">{forecastDay[i]}</label>
+                  <label className="desc">{item.weather[0].description}</label>
+                  <label className="mib-man">{item.main.temp}°C</label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
